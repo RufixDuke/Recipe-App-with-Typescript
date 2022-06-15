3ca49aa08226abded7c405fbcd9b53298f3b9a0a
RufixDuke <rufixduke01@gmail.com>
2022-06-15 13:55:21 +0100

    Starting.....

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [recipesFound, setRecipesFound] = useState([]);
+    const [recipeSearch, setRecipeSearch] = useState("");
+
+    const searchForRecipes = async (query: string): Promise<any> => {
+        const result = await fetch(
+            `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
+        );
+        return (await result.json()).result;
+    };
+    return (
+        <div className="App">
+            <h2>....</h2>
+        </div>
+    );
 }
 
 export default App;
